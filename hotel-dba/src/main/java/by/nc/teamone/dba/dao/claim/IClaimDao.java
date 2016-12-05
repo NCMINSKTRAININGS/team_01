940e34600bba0866b241b92d6710bab6d4661be0
PetrusHahol <petius96@gmail.com>
2016-12-05 20:21:46 +0300

    bug fixed entity truth

---
+++ hotel-dba/src/main/java/by/nc/teamone/dba/dao/claim/IClaimDao.java
@@ -0,0 +1,10 @@
+package by.nc.teamone.dba.dao.claim;
+
+import by.nc.teamone.dba.dao.IBaseDao;
+import by.nc.teamone.entities.Claim;
+
+/**
+ * Created by Asus on 05.12.2016.
+ */
+public interface IClaimDao extends IBaseDao<Claim,Long> {
+}
--- hotel-entities/src/main/java/by/nc/teamone/entities/Claim.java
+++ hotel-entities/src/main/java/by/nc/teamone/entities/Claim.java
@@ -2,6 +2,7 @@ package by.nc.teamone.entities;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Date;
 
 /**
  * Created by Asus on 05.12.2016.
@@ -22,6 +23,14 @@ public class Claim  implements Serializable {
     @OneToOne
     @JoinColumn(name="type_id")
     private Type roomId;
+
+    @Column(name="check-in_date")
+    @Temporal(value=TemporalType.DATE)
+    private Date checkInDate;
+
+    @Column(name="check-out_date")
+    @Temporal(value=TemporalType.DATE)
+    private Date checkOutDate;
 
     public long getId() {
         return id;
@@ -47,5 +56,63 @@ public class Claim  implements Serializable {
     public void setRoomId(Type roomId) {
         this.roomId = roomId;
     }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public Date getCheckInDate() {
+        return checkInDate;
+    }
+
+    public void setCheckInDate(Date checkInDate) {
+        this.checkInDate = checkInDate;
+    }
+
+    public Date getCheckOutDate() {
+        return checkOutDate;
+    }
+
+    public void setCheckOutDate(Date checkOutDate) {
+        this.checkOutDate = checkOutDate;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Claim claim = (Claim) o;
+
+        if (id != claim.id) return false;
+        if (userId != null ? !userId.equals(claim.userId) : claim.userId != null) return false;
+        if (roomId != null ? !roomId.equals(claim.roomId) : claim.roomId != null) return false;
+        if (checkInDate != null ? !checkInDate.equals(claim.checkInDate) : claim.checkInDate != null) return false;
+        return checkOutDate != null ? checkOutDate.equals(claim.checkOutDate) : claim.checkOutDate == null;
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = (int) (id ^ (id >>> 32));
+        result = 31 * result + (userId != null ? userId.hashCode() : 0);
+        result = 31 * result + (roomId != null ? roomId.hashCode() : 0);
+        result = 31 * result + (checkInDate != null ? checkInDate.hashCode() : 0);
+        result = 31 * result + (checkOutDate != null ? checkOutDate.hashCode() : 0);
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        return "Claim{" +
+                "id=" + id +
+                ", userId=" + userId +
+                ", roomId=" + roomId +
+                ", checkInDate=" + checkInDate +
+                ", checkOutDate=" + checkOutDate +
+                '}';
+    }
+
+
 
 }
