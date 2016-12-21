f47b480a73f8004f6bc506fec9f3c609908c47f7
PetrusHahol <petius96@gmail.com>
2016-12-21 16:50:45 +0300

    back up for reanimate

--- hotel-web/src/main/java/by/nc/teamone/web/controllers/UserController.java
+++ hotel-web/src/main/java/by/nc/teamone/web/controllers/UserController.java
@@ -1,13 +1,14 @@
 package by.nc.teamone.web.controllers;
 
 import by.nc.teamone.entities.models.ClaimModel;
-import by.nc.teamone.entities.models.TypeModel;
+import by.nc.teamone.entities.models.CommandModel;
 import by.nc.teamone.services.IFacade;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.MediaType;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.List;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 @RequestMapping(value = "/user")
@@ -17,11 +18,13 @@ public class UserController {
     private IFacade facade;
 
 
-
-    @RequestMapping(value = "/addRequest2", method = RequestMethod.GET, produces = MediaType.APPLICATION_JSON_VALUE )
-    public @ResponseBody List<TypeModel> addRequestGet(){
-        return facade.getAllTypeModel();// можно вообще убрать TypeModel и сделать facade.getAllType , всё будет тоже норм
-    }
+   @RequestMapping(value = "/getCommand", method = RequestMethod.GET)
+    public ModelAndView getCommandPost(@RequestBody CommandModel commandModel){
+       ModelAndView modelAndView = new ModelAndView("definition-user");
+       modelAndView.addObject("command",commandModel.command);
+       modelAndView.addObject("type", facade.getAllType());
+       return modelAndView;
+   }
 
     @RequestMapping(value = "/addRequest", method = RequestMethod.POST)
     public  void addRequestPost(@RequestBody ClaimModel claimModel){
--- hotel-web/src/main/webapp/assets/js/requests/addClaimRequest.js
+++ hotel-web/src/main/webapp/assets/js/requests/addClaimRequest.js
@@ -1,5 +1,3 @@
-
-
 function addClaimRequest() {
     var claim = {
         checkInDate: document.getElementById('checkInDate').value,
@@ -15,6 +13,19 @@ function addClaimRequest() {
         contentType: 'application/json',
         data: JSON.stringify(claim),
         success: alert("OK")
-
     });
+}
+
+
+function addClaimCommand(cmnd) {
+    var command = {
+        command : cmnd
+    };
+    $.ajax({
+        type: 'POST',
+        dataType: 'json',
+        contentType: 'application/json',
+        data: JSON.stringify(command),
+        success: location.href = '/user/getCommand'
+    })
 }
