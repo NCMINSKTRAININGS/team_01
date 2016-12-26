2c9b0644cc05418449b7915103b82bf85df0a424
1Valeria <shpakovskaya.valeriya.14@gmail.com>
2016-12-26 13:42:45 +0300

    before pull

--- hotel-dba/src/main/resources/data.sql
+++ hotel-dba/src/main/resources/data.sql
@@ -22,9 +22,6 @@ INSERT INTO USER(id, login, password, name, surname, email, phone, money, role ,
 INSERT INTO USER(id, login, password, name, surname, email, phone, money, role , enabled)
   VALUES(2, 'derek', 'password', 'Derek', 'Willson', 'derek.14@gmail.com', '80299614860', '10000', 'ROLE_LANDLORD', true);
 
-INSERT INTO USER(id, login, password, name, surname, email, phone, money, role , enabled)
-  VALUES(2, 'valeria1', 'password', 'Valeria', 'Shpakovskaya', 'shpakovskaya.valeriya.14@gmail.com', '+375257245180', '50000', 'ROLE_LANDLORD', true);
-  
 INSERT INTO CLAIM(id, check_in_date, check_out_date, status, type_id, user_id)
 VALUES(1, '31.12.2016', '02.01.2017', 1, 3, 1);
 
